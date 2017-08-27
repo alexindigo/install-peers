c20a195dbedb4e47af8ab541aed9286099623103
Alex Indigo <iam@alexindigo.com>
2017-08-27 01:05:20 -0700

    Cleaned up index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-#!/usr/bin/env node
-
-var path = require('path');
-
-var npmModule = path.resolve(process.env['npm_execpath'], '..');
-//console.log('DEPS', process.env);
-
-console.log('npmModule', npmModule);
-
-
-process.exit(1);
+module.exports = 'This is not the module you are looking for';
